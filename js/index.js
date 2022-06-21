7a50b9cd220f656dead1d02c828ecd3293366bff
Acerbi Nicolas <nicoacerbi4@gmail.com>
2022-06-20 21:49:26 -0300

    Primer entrega del proyecto final realizada

--- js/index.js
+++ js/index.js
@@ -1,59 +1,84 @@
-class Libro{
-    constructor(nombre, categoria, precio){
+class Usuarios{
+    constructor(nombre, contrasenia, categoria){
         this.nombre = nombre.toLowerCase();
-        this.categoria = categoria.toLowerCase();
-        this.precio = parseFloat(precio);
+        this.contrasenia = contrasenia.toLowerCase();
+        this.categoria = parseInt(categoria);
     }
-    getTotalImpuestos() {
-        this.precio = this.precio * 2.2;
-    }
-};
-
-/*
-
-Jorge, el resultado de todo esta en la consola
-
-*/
-
-const libreria = [];
-
-
-libreria.push(new Libro("Padre rico padre pobre", "Educacion financiera",122.50));
-libreria.push(new Libro("padre rico padre pobre", "Educacion financiera",122.50));
-libreria.push(new Libro("El arte de la gUerra", "Desarrollo personal",102.30));
-libreria.push(new Libro("padre Rico padre pobre", "Educacion financiera",122.50));
-libreria.push(new Libro("El arte de la gUerra", "Desarrollo personal",122.50));
-libreria.push(new Libro("Harry potter", "fantasia",220.70));
-
-//muestra 
-calclulaImpuestos(libreria);
-
-// cambiar segundo parametro para buscar si el libro se encuentra en el stock
-buscaStock(libreria, "padre rico padre pobre");
-buscaStock(libreria, "el arte de la guerra");
-buscaStock(libreria, "francesco");
-
-//busca la cantidad de libros que hay en con el mismo nombre
-function buscaStock(libreria, nombre){
-    let contador = 0;
-    for (let i=0; i<libreria.length; i++){
-        if (nombre == libreria[i].nombre) {
-            contador += 1;
+    getPermisos() {
+        if(this.categoria == 0){
+            return "ageno a la plataforma, compre un curso y sea parte de esta familia";
+        }else if(this.categoria == 1){
+            return "alumno";
+        }else if(this.categoria == 2){
+            return "tutor";
+        }else if(this.categoria == 3){
+            return "profesor/a";
         }
     }
-    if(contador == 1){
-        console.log("Tenemos un solo libro llamado " + nombre);
-    }else if (contador > 1) {
-        console.log("Tenemos " + contador + " libros llamados " + nombre);
-    }else{
-        console.log("No tenemos stock del libro " + nombre);
-    }
 };
 
-//calcula y muestra
-function calclulaImpuestos(stock){
-    for(let i=0; i<stock.length; i++){
-        stock[i].getTotalImpuestos();
-        console.log("El libro " + stock[i].nombre.toUpperCase() + " cuesta $"+ stock[i].precio+ " pesos.");
+const usuarios = [];
+usuarios.push(new Usuarios("jorge", "coderhouse12",2));
+usuarios.push(new Usuarios("nicolas", "coderhouse10",1));
+usuarios.push(new Usuarios("silma", "housecoder",3));
+usuarios.push(new Usuarios("ricardo", "coder",0));
+
+let flag = true;
+while (flag) {
+    let nombreIngresado = prompt("Ingrese usuario");
+    if(nombreIngresado != null){
+        //valida que exista el usuario para poder buscar la contrasenia
+        let existe = existeUsuario(nombreIngresado, usuarios);
+        if(existe){
+            ingresaContrasenia(usuarios, nombreIngresado);
+            flag = false;
+        }else if (existe == false) {
+            console.log("no existe el usuario " + nombreIngresado);
+        }
+    }
+}
+
+//Funcion que entra en bucle si la contraseña no es correcta
+function ingresaContrasenia(listaUsuarios, nombre) {
+    for (let i = 0; i < listaUsuarios.length; i++) {
+        const element = listaUsuarios[i].nombre;
+        if (element == nombre) {
+            let bucle = true;
+            while (bucle) {
+                let contrasenia = prompt("Ingresa contraseña");
+                bucle = comparaContrasenia(listaUsuarios[i], contrasenia);
+                if(bucle == false){
+                    alert("Bienvenido " + listaUsuarios[i].nombre + " usted es " + listaUsuarios[i].getPermisos());
+                }
+            }
+        }
+    }
+}
+//Compara contraseña con el nombre de usuario ya ingresado para ver si es correcta
+function comparaContrasenia(usuario, contrasenia){
+    if(usuario.contrasenia == contrasenia){
+        return false;
+    }else{
+        return true;
+    }
+}
+//Valida la existenca del usuario
+function existeUsuario(nombre, listaUsuarios){
+    let salida = false;
+    for (let i = 0; i < listaUsuarios.length; i++) {
+        const element = listaUsuarios[i].nombre;
+        if (element == nombre) {
+            salida = true;
+            break;
+        }
+    }
+    return salida;
+}
+//Muestra lista de usuarios registrados. ------------
+muestraLista(usuarios);
+function muestraLista(listaUsuarios){
+    console.log("----------------------------");
+    for (let i=0; i<listaUsuarios.length; i++){
+        console.log("Usuario " + listaUsuarios[i].nombre + " es " + listaUsuarios[i].getPermisos());
     }
 };
