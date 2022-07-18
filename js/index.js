d06bb348eba5a607266b15cda82245953d7e8d81
Acerbi Nicolas <nicoacerbi4@gmail.com>
2022-07-18 11:05:15 -0300

    Proyecto Final terminado

--- js/index.js
+++ js/index.js
@@ -5,64 +5,133 @@ class Usuarios{
     }
 };
 
-let enviaInfo = document.getElementById("enviarInfo");
+const url = "https://pokeapi.co/api/v2/pokemon/4/";
 
-if (enviaInfo) {
-    enviaInfo.addEventListener("click", cargaDatos);
-}
+fetch(url)
+.then(Response => Response.json())
+.then(data => {
+    let curso = document.getElementById("cursoDe");
+    curso.innerHTML =`<p>${data.name}</p>`
+    
+})
+.catch(err=>console.log(err))
+
+
+let enviaInfo = document.getElementById("enviarInfo");
+enviaInfo ? enviaInfo.addEventListener("click",cargaDatos):{}
+
 const usuario = [];
-function cargaDatos() {
+function cargaDatos(){
     let userNameV = document.getElementById("userName").value;
     let contraV = document.getElementById("contra").value;
     let validaContraV = document.getElementById("validaContra").value;
+    let msjError = document.getElementById("errorValida");
 
-        if (userNameV == "") {
-            document.getElementById("errorLogueoReg").innerHTML = "Ingrese usuario";
-        }else if (contraV == "") {
-            document.getElementById("errorContraReg").innerHTML = "Ingrese su contraseña";
-        }else if (validaContraV == "") {
-            document.getElementById("errorValida").innerHTML = "Ingrese la confirmacion de su contraseña";
-            console.log("Ingrese la validacion de su contraseña")
-        }else if (contraV != validaContraV) {
-            document.getElementById("validaMal").innerHTML = "Las contraseñas no coinciden";
-        }else{
-            document.getElementById("registracionExitosa").innerHTML = "Registrado correctamente";
-            localStorage.setItem(userNameV,contraV)
-        }
-}
-//eliminaLocalStore(true)
-function eliminaLocalStore(espacio) {
-    if (espacio) {
-        localStorage.clear();
+    if (userNameV == "") {
+        Swal.fire({
+            title: 'Error',
+            text: 'Ingrese usuario',
+            icon: 'error',
+            showConfirmButton: false,
+            timer:2000
+        })
+    }else if (contraV == "") {
+        Swal.fire({
+            title: 'Error',
+            text: 'Ingrese su contraseña',
+            icon: 'error',
+            showConfirmButton: false,
+            timer:2000
+        })
+    }else if (validaContraV == "") {
+        Swal.fire({
+            title: 'Error',
+            text: 'Ingrese la confirmacion de su contraseña',
+            icon: 'error',
+            showConfirmButton: false,
+            timer:2000
+        })
+    }else if (contraV != validaContraV) {
+        Swal.fire({
+            title: 'Error',
+            text: 'Las contraseñas no coinciden',
+            icon: 'error',
+            showConfirmButton: false,
+            timer:2000
+        })
     }else{
-        sessionStorage.clear();
+        Swal.fire({
+            title: 'Registrado correctamente',
+            text: 'Listo para el logueo',
+            icon: 'success',
+            showConfirmButton: false,
+            timer:2000
+        })
+        usuario.push(new Usuarios(userNameV,contraV));
+        localStorage.setItem(userNameV,contraV)
     }
 }
 let botonLogueo = document.getElementById("btnlogueo");
-if (botonLogueo) {
-    botonLogueo.addEventListener("click", revisaDB);
-}
+botonLogueo ? botonLogueo.addEventListener("click", revisaDB):{};
+
 function revisaDB() {
     let userLogueo = document.getElementById("usuarioLogueo").value;
     let contraLogueo = document.getElementById("contraLogueo").value;
+    let msj = document.getElementById("userLogueoDiv");
 
-    
     for (let i = 0; i < localStorage.length; i++) {
         let clave = localStorage.key(i)
         let valor = localStorage.getItem(clave);
-    
-        if (userLogueo != clave) {
-            console.log("no es el mismo usuario")
+
+        if (userLogueo == "") {
+            Swal.fire({
+                title: 'Usuario Incompleto',
+                text: 'Ingrese su usuario',
+                icon: 'question',
+                showConfirmButton: false,
+                timer:1500
+            })
+        }else if (userLogueo != clave) {
+            Swal.fire({
+                title: 'Usuario Incorrecto',
+                text: 'Ingrese su usuario',
+                icon: 'question',
+                showConfirmButton: false,
+                timer:1500
+            })
+        }else if (contraLogueo == "") {
+            Swal.fire({
+                title: 'Error',
+                text: 'Ingrese la contraseña',
+                icon: 'question',
+                showConfirmButton: false,
+                timer:1500
+            })
         }else if(contraLogueo != valor){
-            document.getElementById("userLogueoDiv").innerHTML = "Error en el ingreso";
-            console.log("Error de contraseña");
+            Swal.fire({
+                title: 'Error',
+                text: 'Ingrese bien su contraseña',
+                icon: 'error',
+                showConfirmButton: false,
+                timer:1500
+            })
         }else{
-            console.log("Ingresaste");
+            Swal.fire({
+                title: 'Bienvenido',
+                text: '',
+                icon: 'success',
+                showConfirmButton: false,
+                timer:2100
+            })
             ingresaAPlataforma();
             break;
         }
-    }
+    } 
 }
 function ingresaAPlataforma() {
     location.href = "http://127.0.0.1:5500/ProyectoFinal/pages/plataforma.html";
+}
+//eliminaLocalStore(true)
+function eliminaLocalStore(espacio) {
+    espacio? localStorage.clear() : sessionStorage.clear();
 }
